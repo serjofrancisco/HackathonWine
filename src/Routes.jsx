481facb879c33e6e952ec79de9617177a2e67ec4
LDantasA <leonardo.dantas0gmail.com>
2022-07-30 18:27:20 -0300

    feat: :sparkles: página de lojas fisicas criada

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import OurStoresPage from './pages/OurStoresPage';
 
 export default function RoutePaths() {
   return (
     <Routes>
       <Route exact path="/" element={<LandingPage />} />
+      <Route exact path="/lojas" element={<OurStoresPage />} />
     </Routes>
   );
 }
