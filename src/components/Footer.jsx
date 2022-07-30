e9c6341e0fc6f48b3230544d6e150bb9113d17bb
Sergio Francisco <spinheiro.lf@gmail.com>
2022-07-30 13:57:43 -0300

    feat: :art: Adicionando componente Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import wTransparente from '../images/w-transparente.png';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <img src={wTransparente} alt="W" />
+      <div>
+        <h4>W2W E-Commerce de Vinhos S.A.</h4>
+        <span>CNPJ: 09.813.204/0002-05</span>
+      </div>
+      <p>
+        Rua Comendador Alcides Simão Helou, 1478 <br /> Serra - ES - CEP 29168-090
+      </p>
+      <p>
+        SE BEBER, NÃO DIRIJA. APRECIE COM MODERAÇÃO. A VENDA DE BEBIDAS ALCOÓLICAS É PROIBIDA PARA
+        MENORES DE 18 ANOS.
+      </p>
+    </footer>
+  );
 }
