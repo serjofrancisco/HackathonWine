11ed4c186246c53f258218f06e84ef0806f1ebaf
FlavioVillar <flavio_villar@hotmail.com>
2022-07-30 20:59:24 -0300

    style: estilização do header e página principal

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import Header from '../components/Header';
-import WineBanner from '../components/WineBanner';
-import Footer from '../components/Footer';
+// import WineBanner from '../components/WineBanner';
+// import bannerLandingPage from '../../src/images/banner-header-landing-page.jpg';
+// import clubeExplication from '../../src/images/como-funciona-clube.png';
+import vinhoAriki from '../../src/images/vinho-ariki.png';
+// import Footer from '../components/Footer';
+import './styles/LandingPage.css';
 
 export default function LandingPage() {
   return (
     <>
-      <Header />
-      <WineBanner />
-      <Footer />
+      <div className="container-geral-landing-page">
+        <div className="container-landing-page">
+          <Header />
+          <div className="container-geral-banner-landing-page">
+            <div className="container-banner-landing-page">
+              <div className="container-texto-banner-landing-page">
+                <h1>O maior clube de vinho do mundo!</h1>
+                <h3>Se Conect com pessoas...</h3>
+                <h3>Se Conect com mundo...</h3>
+                <h3>por meio da paixão por vinho.</h3>
+              </div>
+              <div className="container-imagem-banner-landing-page">
+                <img className="imagem-vinho-ariki" alt="vinho-ariki" src={vinhoAriki} />
+              </div>
+            </div>
+          </div>
+          <div className="container-main-landing-page">
+            <main>
+              {/* <div className="container-explication-landing-page">
+            <img alt="explication" src={clubeExplication} />
+          </div> */}
+              {/* <WineBanner /> */}
+            </main>
+          </div>
+          {/* <Footer /> */}
+        </div>
+      </div>
     </>
   );
 }
