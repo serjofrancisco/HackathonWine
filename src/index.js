b10fd6a915b877fbb7c4bc42e28f178c6ed479b2
LDantasA <leonardo.dantas0gmail.com>
2022-07-30 14:22:23 -0300

    fix: Provider removido temporiariamente

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Provider from './contexts/Provider';
+// import Provider from './contexts/Provider';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
