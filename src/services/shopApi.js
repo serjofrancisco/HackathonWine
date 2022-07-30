620c653e74e882798c1eb0f5dc053a6493fab1f6
Sergio Francisco <spinheiro.lf@gmail.com>
2022-07-30 11:37:48 -0300

    fazendo chamadas de api

---
+++ src/services/shopApi.js
@@ -0,0 +1,14 @@
+const WINES_URL = 'https://wine-back-test.herokuapp.com/products';
+const CLUB_URL = 'https://wine-club-proxy.herokuapp.com/modalities';
+
+export const getAllWines = async () => {
+  const response = await fetch(WINES_URL);
+  const json = await response.json();
+  return response.ok ? Promise.resolve(json.items) : Promise.reject(json);
+};
+
+export const getClub = async () => {
+  const response = await fetch(CLUB_URL);
+  const json = await response.json();
+  return response.ok ? Promise.resolve(json.plans) : Promise.reject(json);
+};
