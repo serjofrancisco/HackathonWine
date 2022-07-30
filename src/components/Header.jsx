a3b38282dbd6fede3e5b9ec47ba287a026e38ac4
LDantasA <leonardo.dantas0gmail.com>
2022-07-30 13:29:38 -0300

    feat: Estrutura do Header criada

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import wineLogo from '../images/wine.svg';
+import { Link } from 'react-router-dom';
+import { BsSearch } from 'react-icons/bs';
 
-export default function Header() {
-  return <div>Header</div>;
+function Header() {
+  return (
+    <header className="header">
+      <Link to="/">
+        <img alt="Wine" src={wineLogo} />
+      </Link>
+      <Link to="/clube">
+        <span>Clube</span>
+      </Link>
+      <Link to="/loja">
+        <span>Loja</span>
+      </Link>
+      <BsSearch />
+    </header>
+  );
 }
+
+export default Header;
