5124394118133e9a89a487de9d9011cef82d9576
FlavioVillar <flavio_villar@hotmail.com>
2022-07-31 11:36:53 -0300

    style(estilização carousel vinhos):

--- src/components/WineCard.jsx
+++ src/components/WineCard.jsx
@@ -3,43 +3,27 @@ import './styles/WineCard.css';
 import { putOnCard } from '../helpers/helpersWines';
 
 export default function WineCard(wine) {
-  const {
-    name,
-    image,
-    price,
-    discount,
-    priceMember,
-    priceNonMember,
-    page,
-    // type,
-    // classification,
-    // size,
-    // rating,
-    // avaliations,
-    country,
-    // region,
-    flag
-    // sommelierComment
-  } = wine.wine || wine;
+  const { name, image, price, priceMember, page, country, flag } = wine.wine || wine;
   return (
     <div className="container-geral-wine-card">
-      {/* <div className="container-wine-card" data-testid="winecard"> */}
       <div className="container-nome-flag-wine">
-        <img className="imagem-winecard-bandeira" src={flag} alt={country} />
-        <img className="imagem-winecard-vinho" src={image} alt={name} />
-        {/* </div> */}
+        <div className="container-img-carousel">
+          <img className="imagem-winecard-bandeira" src={flag} alt={country} />
+          <img className="imagem-winecard-vinho" src={image} alt={name} />
+        </div>
         <h3 className="wine-name-WineCard">{name}</h3>
         <div className="container-wine-card-prices">
           <span></span>
-          <span>R${price}</span>
-          <span>R${discount}</span>
-          <span>Preço - R${priceNonMember}</span>
-          <span>Preço para Membros- R${priceMember}</span>
+          <span>
+            R$ <a>{price.toFixed(2)} </a>{' '}
+          </span>
+          <br />
+          <span className="texto-membro">
+            Preço para Membros <br />
+            <a className="preco-membro"> R$ {priceMember.toFixed(2)}</a>
+          </span>
         </div>
-        <div className="container-wine-card-rating">
-          {/* <span>Avaliações:{avaliations}</span> */}
-          {/* <p>{sommelierComment}</p> */}
-        </div>
+        <div className="container-wine-card-rating"></div>
         {page && <button onClick={() => putOnCard(wine)}>Comprar</button>}
       </div>
     </div>
